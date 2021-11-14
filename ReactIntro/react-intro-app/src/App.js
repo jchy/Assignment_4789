4ae4d9b255633f1bdc87ef673a65ceb6aa240967
Jaswant Chaudhary <jaswantchaudhary@Jaswants-MacBook-Air.local>
2021-11-14 21:48:18 +0530

    React Intro problem's solution file

---
+++ ReactIntro/react-intro-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateCard from './Components/card'
+
+function App() {
+  return (
+    <div className="App">
+      <CreateCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactIntro/react-intro-app/src/Components/card.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const CreateCard = ()=>{
+    return <div>
+        <h1>Creating a React app</h1>
+        <ul>
+            <li>if you don't have the create-react app intalled globally install it first using the follwing Command</li>
+            <ul>
+                <li>npx install -g create-react-app</li>
+            </ul>
+            <li>if you have the create-react app intalled globally istall then the follwing Command</li>
+            <ul>
+                <li>create-react-app react-app-name</li>
+            </ul>
+        </ul>
+        <h3>SRC Folder</h3>
+        <ul>
+            <li>Src folder will our playgound folder where we will be doing all the stuffs</li>
+            <ul>
+                <li>in app.js folder import all the files that you create for the app and assemble it and render it</li>
+            </ul>
+        </ul>
+        <h3>Public Folder</h3>
+        <ul>
+            <li>It is the pulic folder which will containe 2 logo images for react by default</li>
+            <ul>
+                <li>index.html is the only folder in the Public folder which contain the html file.</li>
+            </ul>
+        </ul>
+        
+        <h3>package.json</h3>
+        <ul>
+            <li>It is the folder which will contain all the dependencies intalled in the react app</li>
+        </ul>
+
+        <h3>Start the project</h3>
+        <ul>
+            <li>To start any project run the following command</li>
+            <ul>
+                <li>npm start</li>
+            </ul>
+        </ul>
+
+        <h3>Stop the project</h3>
+        <ul>
+            <li>To stop the project rpress ctrl+z| ctrl+c</li>
+        </ul>
+
+        <h3>HMR</h3>
+        <ul>
+            <li>It is abbrebiation of the "HOT MODULE REPLACEMENT</li>
+        </ul>
+
+        <h3>JSX</h3>
+        <ul>
+            <li>JSX is stand for the javascript and xml</li>
+            <li>It the simple for the rect to understand it and render it</li>
+        </ul>
+
+        
+        <h3>babel</h3>
+        <ul>
+            <li>babel is a tool used for the </li>
+            <li>It the simple for the rect to understand it and render it</li>
+        </ul>
+
+
+
+    </div>
+} 
+export default CreateCard;
